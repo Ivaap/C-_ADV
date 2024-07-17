29a365e72bf8c3c130f7d3185dbcfe67b664fe91
Iva Apostolova <137160725+Ivaap@users.noreply.github.com>
2024-07-17 22:00:02 +0300

    Add files via upload

---
+++ SetsAndDictionaries_Wardrobe.cs
@@ -0,0 +1,47 @@
+﻿namespace SetsAndDictionaries_Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                string[] inputArg = Console.ReadLine().Split("-> ");
+                string color = inputArg[0];
+                string[] clothes = inputArg[1].Split(",");
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+                foreach(var item in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(item))
+                    {
+                        wardrobe[color].Add(item, 0);
+                    }
+                    wardrobe[color][item]++;
+                }
+            }
+            string[] targetClothes = Console.ReadLine().Split();
+
+            foreach(var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+                foreach(var (clothe, value ) in color.Value)
+                {
+                    if(clothe == targetClothes[1] && color.Key == targetClothes[0])
+                    {
+                        Console.WriteLine($"* {clothe} - {value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"*{clothe} - {value}");
+                    }
+                }
+            }
+        }
+    }
+}
